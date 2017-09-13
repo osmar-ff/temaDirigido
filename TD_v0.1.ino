cb8ed88fccb1a4654434b09f212b565572a2cea2
Osmar Ferreira de Freitas <31131765+osmar-ff@users.noreply.github.com>
2017-09-13 10:48:58 -0300

    Change cmd codes, String object
    Implemented new variables to evaluate if the code should be changed or compared. Also the way that the codes were stored in memory were changed from char arrays to String object. Some others minors changes in the code were also implemented.

--- TD_v0.1.ino
+++ TD_v0.1.ino
@@ -5,10 +5,20 @@
 #include <RFremote.h>
 
 
-const char *cmd1 = "0110100100100110100100100100110100100100110110110110100100110110110110110100110100110";
-const char *cmd2 = "0110100100100110100100100100110100100100110110110110100100110110110110100100110100110";
-const char *cmd3 = "0110100100100110100100100100110100100100110110110110100100110110110100110100110100110";
+String cmd1 = "0110100100100110100100100100110100100100110110110110100100110110110110110100110100110";
+char cmd1c []= "0110100100100110100100100100110100100100110110110110100100110110110110110100110100110";
+String cmd2 = "0110100100100110100100100100110100100100110110110110100100110110110110100100110100110";
+char cmd2c[] = "0110100100100110100100100100110100100100110110110110100100110110110110110100110100110";
+String cmd3 = "0110100100100110100100100100110100100100110110110110100100110110110100110100110100110";
+char cmd3c[] = "0110100100100110100100100100110100100100110110110110100100110110110110110100110100110";
+String cmdIn = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 SignalPatternParams params;
+
+int eval1=0;
+int eval2=0;
+int eval3=0;
+
+char receive;
 
 RFrecv rfrecv;
 
@@ -36,47 +46,88 @@ void loop()
 {
   if (rfrecv.available())
   {
-    if (strncmp((char*)rfrecv.cmd, cmd1, CMD_SIZE) == 0)
-    {
-      Serial.print("A");
+    cmdIn=String((char*)rfrecv.cmd);
+    if (eval1==0){
+      if (cmdIn==cmd1){
+        Serial.print("A");
+      }
     }
-    else
-    {
-      if (strncmp((char*)rfrecv.cmd, cmd2, CMD_SIZE) == 0)
-    {
-      Serial.print("B");
+    else if (eval1==1){
+      cmd1=cmdIn;
+      Serial.print("G");
     }
-    else
-    {
-      if (strncmp((char*)rfrecv.cmd, cmd3, CMD_SIZE) == 0)
-    {
-      Serial.print("C");
+
+    if (eval2==0){
+      if (cmdIn==cmd2){
+        Serial.print("B");
+      }
     }
-    else
-    {
-      Serial.print("ERRO!");
+    else if (eval2==1){
+      cmd2=cmdIn;
+      Serial.print("H");
     }
+    
+    if (eval3==0){
+      if (cmdIn==cmd3){
+        Serial.print("C");
+      }
     }
+    else if (eval3==1){
+      cmd3=cmdIn;
+      Serial.print("I");
     }
+    
+    
+    
   }
-  String t;
   while(Serial.available()){
-  t+=(char)Serial.read();
+  receive=Serial.read();
   }
-  if(t.length()) { //if string is not empty do the following
-    if(t == "a") { //if the string is equal to "on" then turn LED on
-      vw_send("0110100100100110100100100100110100100100110110110110100100110110110110110100110100110", 85);
-    Serial.write("a");
+  if(receive>0) { 
+    cmd1.toCharArray(cmd1c,cmd1.length());
+    cmd2.toCharArray(cmd2c,cmd2.length());
+    cmd3.toCharArray(cmd3c,cmd3.length());
+    
+    if(receive == 'a') { //if the string is equal to "on" then turn LED on
+      vw_send((uint8_t *)cmd1c, 85);
+       Serial.write("a");
+       receive=0;
     }
-    else if (t == "b") { 
-      vw_send("0110100100100110100100100100110100100100110110110110100100110110110110100100110100110", 85);
-      Serial.write("b");
+    else if (receive == 'b') { 
+      vw_send((uint8_t *)cmd2c, 85);
+      //Serial.write("b");
+      receive=0;
     }
-    else if (t=="c"){
-       vw_send("0110100100100110100100100100110100100100110110110110100100110110110100110100110100110", 85);
-        Serial.write("c"); 
-  
-  }
+    else if (receive == 'c'){
+       vw_send((uint8_t *)cmd3c, 85);
+       // Serial.write("c");
+        receive=0;
+    }
+    else if (receive == 'g'){
+       eval1=1;
+       receive=0;
+    }
+    else if (receive == 'h'){
+       eval2=1;
+       receive=0;
+    }
+    else if (receive == 'i'){
+       eval3=1;
+       receive=0;
+    }
+    else if (receive == 't'){
+       eval1=0;
+       receive=0;
+    }
+    else if (receive == 'u'){
+       eval2=0;
+       receive=0;
+    }
+    else if (receive == 'v'){
+       eval3=0;
+       receive=0;
+    }
+   
   }
 }
 
